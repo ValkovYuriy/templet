7a495f96a695de0dd5264b54bf43146291945663
ValkovYuriy <139258303+ValkovYuriy@users.noreply.github.com>
2023-12-11 14:02:26 +0400

    Update and rename start4python2.sh to start4python3.sh

--- bin/start4python3.sh
+++ bin/start4python3.sh
@@ -8,21 +8,7 @@ PID_FILE=agent.pid
 if [ -f $PID_FILE ] && [ -f /proc/$(cat $PID_FILE)/status ]; then
    echo "Agent is already running"
 else
-    #if which python3 > /dev/null; then
-        #PYTHON=python3
-    #else
-        # Find latest Python 2.x version
-        #PYTHON=$(ls /usr/bin/python2.? | tail -n1)
-    #fi
-    
     PYTHON=/srv/conda/envs/everest/bin/python3
-    
-    # Make sure Python is 2.6 or later
-    PYTHON_OK=`$PYTHON -c 'import sys; print (sys.version_info >= (2, 6) and "1" or "0")'`
-    if [ "$PYTHON_OK" = '0' ]; then
-        echo "Python versions < 2.6 are not supported"
-        exit 1
-    fi
     PYTHONPATH=$(dirname $0)/..:$PYTHONPATH $PYTHON -m everest_agent.start 2>errors.txt $@ &
     echo $! > $PID_FILE
     echo "Agent is started"
